b83e27b2e3db3b08a642a9855103e99e45c5fcf8
Aaron Wiedemer <wiedemer.7@osu.edu>
2024-04-13 22:18:56 -0400

    added script that gets data. Editied .gitignore

---
+++ scripts/get_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=PAS2700
+#SBATCH --time 4:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mail-type=END,FAIL
+
+set -euo pipefail
+
+# load conda
+module load miniconda3/24.1.2-py310
+
+# activate fastq-dl for downloading data
+conda activate /fs/ess/PAS2700/users/awiedemer673/class_project/fastq-dl
+
+# get da data
+fastq-dl --accession SRP083099 --provider sra --outdir /fs/ess/PAS2700/users/awiedemer673/class_project/data/fastqc --only-provider  
