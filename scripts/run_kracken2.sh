18bbde8b5392fcef42074d3a98e5f19f0595d92a
Aaron Wiedemer <wiedemer.7@osu.edu>
2024-04-15 13:18:41 -0400

    created ./scripts/run_kracken2.sh which runs kraken2 on the input data

---
+++ scripts/run_kracken2.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+#SBATCH --account=PAS2700
+#SBATCH --time 4:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --mail-type=END,FAIL
+
+set -euo pipefail
+
+# get in the right place
+cd /fs/ess/PAS2700/users/$USER/class_project/
+
+# input vaiables
+DBNAME=$1
+INPUTDATA=$2
+
+#check if input is correct
+if [[ ! "$#" -eq 2 ]]; then
+    echo "Error: Requires kraken2 database dir and input data dir containing .fastq.gz files"
+    exit 1
+fi
+
+# output variables - automatically creates
+
+REPORTDIR=./kraken2/kraken_reports
+
+OUTDIR=./kraken2/kraken_outputs
+
+# make dirs
+mdkir -p $REPORTDIR $OUTDIR
+
+# load conda
+module load miniconda3/24.1.2-py310
+
+# Check if the ./conda/kraken2 directory exists
+if [ ! -d "./conda/kraken2" ]; then
+    echo "Directory ./conda/kraken2 does not exist. Creating conda environment."
+    conda create -y -p ./conda/kraken2 -c bioconda kraken2
+else
+    echo "./conda/kraken2 already exists."
+fi
+
+# activate kraken2
+conda activate ./conda/kraken2   
+
+
+# run kraken2
+
+for file in "$INPUTDATA"/*.fastq.gz; do
+
+    # set location
+    REPORTOUT="$REPORTDIR"/"$(basename "$file" .fastq.gz)".k2report
+    OUTPUT="$OUTDIR"/"$(basename "$file" .fastq.gz)".kraken2
+
+    # report
+    echo "Input file: $file"
+    echo "Report file: $REPORTOUT" 
+    echo "Output file: $OUTPUT"
+
+    # run kraken2
+
+   kraken2 --db $DBNAME --gzip-compressed --report $REPORTOUT --report-minimizer-data --minimum-hit-groups 3 --threads 10 $file > $OUTPUT
+done
