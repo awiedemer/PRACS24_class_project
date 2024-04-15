6ec8207f66bbce48dc3eb75a095e68b2df1048de
Aaron Wiedemer <wiedemer.7@osu.edu>
2024-04-15 13:17:56 -0400

    modified kraken2 setup to now require a database output in its arugments

--- scripts/setup_kraken2_databases.sh
+++ scripts/setup_kraken2_databases.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 #SBATCH --account=PAS2700
-#SBATCH --time 4:00:00
-#SBATCH --cpus-per-task=10
+#SBATCH --time 8:00:00
+#SBATCH --cpus-per-task=18
 #SBATCH --mail-type=END,FAIL
 
 #saftey first
 set -euo pipefail
 
 # get in the right place
-cd /fs/ess/PAS2700/users/awiedemer673/class_project/
+cd /fs/ess/PAS2700/users/$USER/class_project/
+
+# variables
+DBNAME=$1
+
+#check if input is correct
+if [[ ! "$#" -eq 1 ]]; then
+    echo "Error: Requires database output dir"
+    exit 1
+fi
+
+# Initial reporting
+echo "# Starting script setup_kraken2_databases"
+date
+echo "# Output kraken2 database dir:          $DBNAME"
+echo
 
 # load conda
 module load miniconda3/24.1.2-py310
 
-# Check if the ./conda/kraken2 directory exists
+# Check if the kraken2 is in the conda dir
 if [ ! -d "./conda/kraken2" ]; then
     echo "Directory ./conda/kraken2 does not exist. Creating conda environment."
     conda create -y -p ./conda/kraken2 -c bioconda kraken2
@@ -24,9 +39,6 @@ fi
 # activate kraken2
 conda activate ./conda/kraken2
 
-# variables
-DBNAME=./kraken2/database
-
 # Check if the database file exists, and create if not
 if [ ! -f "$DBNAME" ]; then
     echo "Database file $DBNAME does not exist. Creating directory and building database."
@@ -34,10 +46,12 @@ if [ ! -f "$DBNAME" ]; then
     kraken2-build --standard --db "$DBNAME"
 elif [ ! -s "$DBNAME" ]; then
     echo "Database file $DBNAME is empty. Building database."
-    kraken2-build --standard --db "$DBNAME"
+    kraken2-build --standard --threads 18 --db "$DBNAME"
 else
     echo "Kraken2 database is already built"
 fi
 
-# set up kraken2 databases
-kraken2-build --standard --threads 10 --db $DBNAME
+# Final reporting
+echo 
+echo "# Finished script setup_kraken2_databases"
+date
